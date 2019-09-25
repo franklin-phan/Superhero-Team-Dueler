d17e9f4b14b339d3ced2e8ffc53590fc37eec194
FRANKLIN_PHAN <franklin.phan123@gmail.com>
2019-09-24 17:59:37 -0700

    Added Armor class

--- superheroes.py
+++ superheroes.py
@@ -5,10 +5,23 @@ class Ability:
         self.max_damage = attack_strength
         pass
     def attack(self):
-        self.attack_strength = random.randint(0,100)
+        self.attack_strength = random.randint(0,self.max_damage)
         return self.attack_strength
-        
+    
+class Armor:
+    def __init__(self,name,max_block):
+        self.name = name
+        self.max_block = max_block
+        pass
+    def block(self):
+        self.max_block = random.randint(0,self.max_block)
+        return self.max_block
+
 if __name__ == "__main__":
     ability = Ability("Debugging Ability", 20)
     print(ability.name)
     print(ability.attack())
+
+    armor = Armor("Cup of Steel",50)
+    print(armor.name)
+    print(armor.block())
