b77536ed4265f2eab59662f9dc2b8560b668220f
FRANKLIN_PHAN <franklin.phan123@gmail.com>
2019-09-29 22:18:23 -0700

    Completed steps 1 and 2

--- superheroes.py
+++ superheroes.py
@@ -23,20 +23,49 @@ class Hero:
         self.abilities = []
         self.armors = []
         self.starting_health = starting_health
-        self.current_health = self.starting_health
-
-
-
+        self.current_health = starting_health
+        pass
+    def add_ability(self,ability):
+        self.abilities.append(ability)
+    def attack(self):
+        damage = 0
+        for ability in self.abilities:
+            damage += ability.attack()
+        return damage
+    def add_armor(self,armor):
+        self.armors.append(armor)
+    def defend(self):
+        defense = 0
+        for armor in self.armors:
+            defense += armor.block()
+        return defense
+    def take_damage(self, damage):
+        take_damage= damage - self.defend()
+        self.current_health -= take_damage
+    def is_alive(self):
+        if self.current_health > 0:
+            return True
+        else:
+            return False
+    def fight(self,opponent):
+        while (self.current_health > 0 and opponent.current_health > 0):
+            self.take_damage(opponent.attack())
+            opponent.take_damage(self.attack())
+        if self.current_health > 0:
+            print(self.name + " wins!")
+        else:
+            print(opponent.name + " wins!")
 
 if __name__ == "__main__":
-    ability = Ability("Debugging Ability", 20)
-    print(ability.name)
-    print(ability.attack())
 
-    armor = Armor("Cup of Steel",50)
-    print(armor.name)
-    print(armor.block())
-
-    my_hero = Hero("Grace Hopper",200)
-    print(my_hero.name)
-    print(my_hero.current_health)
+    hero1 = Hero("Wonder Woman")
+    hero2 = Hero("Dumbledore")
+    ability1 = Ability("Super Speed", 300)
+    ability2 = Ability("Super Eyes", 130)
+    ability3 = Ability("Wizard Wand", 80)
+    ability4 = Ability("Wizard Beard", 20)
+    hero1.add_ability(ability1)
+    hero1.add_ability(ability2)
+    hero2.add_ability(ability3)
+    hero2.add_ability(ability4)
+    hero1.fight(hero2)
